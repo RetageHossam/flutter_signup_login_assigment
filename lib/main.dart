a3f134525e6329dbb6374a1bb8d116bcb4e6c8b3
RetageHossam <hossamretage0@gmail.com>
2025-08-03 14:44:21 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:login_signup_assigment/Login.dart';
 import 'package:login_signup_assigment/Signup.dart';
-import 'package:login_signup_assigment/home.dart';
+
 
 void main() {
   runApp(LoginRun());
@@ -13,12 +13,9 @@ class LoginRun extends StatelessWidget {
     // TODO: implement build
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      // home: Home(),
+     
       initialRoute: Login.x,
       routes: {Login.x: (context) => Login(), Signup.x: (context) => Signup()},
     );
   }
 }
-// {Map<String, Widget Function(BuildContext
-// String dah el key byt3y wy  dah value ely ht3g3 Widget Function(BuildContext)
-// key : value 4kl map wy routes:{}
